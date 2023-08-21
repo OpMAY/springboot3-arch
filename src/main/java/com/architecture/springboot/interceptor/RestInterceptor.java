31b00b98e354d68578044a8fcf0386c733f94978
OpMAY <kevin8622@naver.com>
2023-08-21 18:19:10 +0900

    interceptor jwt validation add

--- src/main/java/com/architecture/springboot/interceptor/RestInterceptor.java
+++ src/main/java/com/architecture/springboot/interceptor/RestInterceptor.java
@@ -2,17 +2,35 @@ package com.architecture.springboot.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Log4j2
 @Component
+@PropertySource("classpath:application.properties")
+@RequiredArgsConstructor
 public class RestInterceptor implements HandlerInterceptor {
+    private final Environment environment;
+    private String accessKey;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return true;
+        boolean isApiTokenValidationOn = Boolean.getBoolean(environment.getProperty("server.settings.api-jwt"));
+        accessKey = environment.getProperty("server.settings.accesskey");
+        if (isApiTokenValidationOn) {
+            String token = request.getHeader(HttpHeaders.AUTHORIZATION)
+                    .substring(request.getHeader(HttpHeaders.AUTHORIZATION)
+                            .lastIndexOf("bearer ") + 7);
+            return true;
+        } else {
+            return true;
+        }
     }
 
     @Override
